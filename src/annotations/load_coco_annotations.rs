5718811deb8e8bff32de012c2ab837e9dfed09fc
Hoel Bagard <hoel.bagard@gmail.com>
2022-12-19 21:48:04 +0900

    Got the json loading part working =)

--- src/annotations/load_coco_annotations.rs
+++ src/annotations/load_coco_annotations.rs
@@ -1,6 +1,20 @@
+use crate::annotations;
 use crate::annotations::coco_types::Dataset;
+use std::fs;
+use std::io::ErrorKind;
 use std::path::Path;
 
-pub fn load_json(annotations_path: &Path) {
-    println!("{:?}", annotations_path)
+pub fn load_json(annotations_path: &String) {
+    println!("{:?}", annotations_path);
+    let annotations_file_content = fs::read_to_string(annotations_path).unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            panic!("Could not find the annotations file: {:?}", error);
+        } else {
+            panic!("Problem opening the annotations file: {:?}", error);
+        }
+    });
+
+    let dataset: Dataset =
+        serde_json::from_str(&annotations_file_content).expect("Error decoding the json file");
+    println!("{:?}", dataset.annotations[0]);
 }
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     match args.command_type {
         args::CommandType::Visualize(visualize_command) => match visualize_command.command {
             args::VisualizeSubcommand::VisualizeSample(sample_args) => {
-                load_json(Path::new(&sample_args.annotation_file));
+                load_json(&sample_args.annotation_file);
                 1
             }
             args::VisualizeSubcommand::VisualizeAll(dataset_paths) => 1,
