e16bdeaae117b6718494209dd11b221a56e682b9
Hoel Bagard <hoel.bagard@gmail.com>
2022-12-31 00:22:01 +0900

    Simplified code.

--- src/visualize/segmentation.rs
+++ src/visualize/segmentation.rs
@@ -7,31 +7,12 @@ pub type Mask = image::GrayImage;
 impl From<&coco_types::RLE> for Mask {
     /// Converts a RLE to its uncompressed mask.
     fn from(rle: &coco_types::RLE) -> Self {
-        // let mut current_value = 0u8;
-        // let mut current_position = 0u32;
-
-        // let mut flat_mask = vec![0u8; usize::try_from(rle.size[1] * rle.size[0]).unwrap()];
-        // for nb_pixels in &rle.counts {
-        //     for i in current_position..current_position + nb_pixels {
-        //         flat_mask[i as usize] = current_value
-        //     }
-        //     current_value = if current_value == 0 { 1 } else { 0 };
-        //     current_position += nb_pixels;
-        // }
-        // The from raw would need to use Fortran order for it to work.
-        // let mut mask = ImageBuffer::from_raw(rle.size[1], rle.size[0], flat_mask).unwrap();
-        // for Luma([pixel]) in mask.pixels_mut() {
-        //     println!("{:?}", pixel);
-        //     *pixel = *pixel * 255u8;
-        // }
-
         let mut mask = ImageBuffer::new(rle.size[1], rle.size[0]);
         let mut current_value = 0u8;
-        let mut current_position = 0u32;
         let mut x = 0u32;
         let mut y = 0u32;
         for nb_pixels in &rle.counts {
-            for _ in current_position..current_position + nb_pixels {
+            for _ in 0..*nb_pixels {
                 mask.put_pixel(x, y, Luma([current_value * 255]));
                 y += 1;
                 if y == rle.size[0] {
@@ -40,7 +21,6 @@ impl From<&coco_types::RLE> for Mask {
                 }
             }
             current_value = if current_value == 0 { 1 } else { 0 };
-            current_position += nb_pixels;
         }
         mask
     }
