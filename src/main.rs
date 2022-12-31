c4033ee5b197618060592513d331a89460d66c04
Hoel Bagard <hoel.bagard@gmail.com>
2022-12-31 20:55:17 +0900

    Renamed the args parsing file to argparse to avoid clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 mod annotations;
 use crate::annotations::load_coco_annotations::load_json;
 
-mod args;
+mod argparse;
 mod visualize;
 
 use clap::Parser;
 
 fn main() {
-    let args = args::COCOtoolsArgs::parse();
+    let args = argparse::COCOtoolsArgs::parse();
 
     match args.command_type {
-        args::CommandType::Visualize(visualize_command) => match visualize_command.command {
-            args::VisualizeSubcommand::VisualizeSample(sample_args) => {
+        argparse::CommandType::Visualize(visualize_command) => match visualize_command.command {
+            argparse::VisualizeSubcommand::VisualizeSample(sample_args) => {
                 let dataset = load_json(&sample_args.annotations_file);
                 visualize::visualize_sample(
                     &dataset,
@@ -19,8 +19,8 @@ fn main() {
                     sample_args.sample_id,
                 );
             }
-            args::VisualizeSubcommand::VisualizeAll(_dataset_paths) => (),
+            argparse::VisualizeSubcommand::VisualizeAll(_dataset_paths) => (),
         },
-        args::CommandType::Split(_split_args) => (),
+        argparse::CommandType::Split(_split_args) => (),
     };
 }
