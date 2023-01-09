98280edd527f74170e96a0e01ed3d5e3928ac232
Hoel Bagard <hoel.bagard@gmail.com>
2023-01-09 22:11:23 +0900

    Began experimenting with minifb.

--- src/visualize.rs
+++ src/visualize.rs
@@ -7,6 +7,52 @@ use image::io::Reader as ImageReader;
 use rand::Rng;
 use std::path::Path;
 
+extern crate image;
+extern crate minifb;
+
+use image::*;
+use minifb::{Key, Window, WindowOptions};
+use std::cmp;
+
+pub trait DrawExt {
+    fn draw_to_buffer(&self, dst: &mut [u32], dst_width: usize, offset: (i32, i32));
+}
+
+impl DrawExt for RgbImage {
+    fn draw_to_buffer(&self, dst: &mut [u32], dst_width: usize, offset: (i32, i32)) {
+        let dst_size = (dst_width as i32, (dst.len() / dst_width) as i32);
+
+        let (width, height) = self.dimensions();
+
+        // Make sure only the pixels get rendered that are inside the dst
+        let min_x = cmp::max(-offset.0, 0);
+        let min_y = cmp::max(-offset.1, 0);
+
+        let max_x = cmp::min(dst_size.0 - offset.0, width as i32);
+        let max_y = cmp::min(dst_size.1 - offset.1, height as i32);
+
+        for y in min_y..max_y {
+            for x in min_x..max_x {
+                let pixel = self.get_pixel(x as u32, y as u32);
+
+                // Convert pixel to Color
+                let raw = 0xFF000000
+                    | ((pixel[0] as u32) << 16)
+                    | ((pixel[1] as u32) << 8)
+                    | (pixel[2] as u32);
+
+                // Apply the offsets
+                let dst_x = (x + offset.0) as usize;
+                let dst_y = (y + offset.1) as usize;
+
+                // Calculate the index
+                let index = dst_x + dst_y * dst_size.0 as usize;
+                dst[index] = raw;
+            }
+        }
+    }
+}
+
 /// # Panics
 ///
 /// Will panic if it cannot read the image file corresponding to the `img_id`.
@@ -19,7 +65,8 @@ pub fn visualize_img(
     image_folder: &String,
     img_id: u32,
 ) -> Result<(), errors::MissingIdError> {
-    let sample_path = Path::new(image_folder).join(&dataset.get_img(img_id)?.file_name);
+    let img_name = &dataset.get_img(img_id)?.file_name;
+    let sample_path = Path::new(image_folder).join(img_name);
 
     let mut img = ImageReader::open(&sample_path)
         .unwrap_or_else(|error| {
@@ -47,10 +94,26 @@ pub fn visualize_img(
         segmentation::draw_mask(&mut img, &mask, color);
     }
 
-    // Use show_image or viuer here.
-    img.save("outputs/out.jpg").unwrap_or_else(|error| {
-        panic!("Could not save the image: {:?}", error);
+    let img_width = img.width() as usize;
+    let img_height = img.height() as usize;
+    let mut buffer: Vec<u32> = vec![0x00FFFFFF; img_width * img_height];
+    img.draw_to_buffer(&mut buffer, img_width, (0, 0));
+
+    let mut window = Window::new(
+        format!("{img_name} - Press Q or ESC to exit",).as_str(),
+        img_width,
+        img_height,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("Could not open window, got the following error: {}", e);
     });
+
+    while window.is_open() && !window.is_key_down(Key::Escape) && !window.is_key_down(Key::Q) {
+        window
+            .update_with_buffer(&buffer, img_width, img_height)
+            .unwrap();
+    }
 
     Ok(())
 }
