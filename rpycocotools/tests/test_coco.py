ad37cac82f0eecc1d164abeb26b791f6c6e7eef5
Hoel Bagard <hoel.bagard@gmail.com>
2023-02-03 19:37:26 +0900

    Added some (very) basic tests.

---
+++ rpycocotools/tests/test_coco.py
@@ -0,0 +1,22 @@
+import rpycocotools
+import pytest
+
+
+@pytest.mark.xfail(reason="Not properly implemented yet")
+def test_modify_dataset_cat(coco_dataset: rpycocotools.COCO):
+    assert coco_dataset.cats[1].name == "person"
+    coco_dataset.cats[1].name = "elf"
+    assert coco_dataset.cats[1].name == "elf"
+
+
+def test_set_dataset_cats(coco_dataset: rpycocotools.COCO):
+    cat = rpycocotools.Category(1, "test", supercategory="super_test")
+    coco_dataset.cats = {1: cat}
+    assert coco_dataset.cats == {1: cat}
+
+
+import rpycocotools
+def test_access_cat(coco_dataset: rpycocotools.COCO):
+    assert coco_dataset.cats[2].supercategory == "vehicle"
+    assert coco_dataset.cats[2].id == 2
+    assert coco_dataset.cats[2].name == "bicycle"
