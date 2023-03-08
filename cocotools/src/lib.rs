c62867678b644c722a2c296b1611cb9a086c0269
Hoel Bagard <hoel.bagard@gmail.com>
2023-03-08 18:03:07 +0900

    Working on API/doc, WIP.

--- cocotools/src/lib.rs
+++ cocotools/src/lib.rs
@@ -1,5 +1,25 @@
+//! The `cocotools` crate provides tools to load, manipulate, convert and visualize COCO format datasets.
+
+//! The base classe is the **[`COCO`]** struct, usually load from a COCO json annotation file.
+//!
+//! This crate aims to provide similar functionalities to the [python pycocotools package](https://pypi.org/project/pycocotools/) / [cocoapi](https://github.com/cocodataset/cocoapi) with additionnal utilities such as conversion between dataset formats. It also aims to have a better documentation and a more readable implementation.
+//!
+//! ## Usage example
+//!
+//! ```
+//!
+//! let dataset = coco::load_anns(annotations_file)?;
+//! ```
+//!
+//!
+//!
+//!
+//!
+
 pub mod annotations;
 pub mod argparse;
 pub mod converters;
 pub mod errors;
 pub mod visualize;
+
+pub use crate::annotations::coco::HashmapDataset as COCO;
---
+++ cocotools/tests/load_coco.rs
@@ -0,0 +1,10 @@
+use std::path::PathBuf;
+
+use cocotools::COCO;
+
+#[test]
+fn load_from_file() {
+    let path = PathBuf::from("../data_samples/coco_25k/annotations.json");
+
+    assert_eq!(a, array![[0., 1., 2., 3., 1.], [4., 5., 6., 7., 2.]]);
+}
