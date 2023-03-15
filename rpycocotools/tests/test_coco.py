2fed2eb12179d356d75721505998877cf957b5df
Hoel Bagard <hoel.bagard@gmail.com>
2023-03-15 23:53:21 +0900

    Added rpycocotools-stubs dependency and pyright config.

--- rpycocotools/tests/test_coco.py
+++ rpycocotools/tests/test_coco.py
@@ -3,19 +3,19 @@ import rpycocotools
 
 
 @pytest.mark.xfail(reason="Not properly implemented yet")
-def test_modify_dataset_cat(coco_dataset: rpycocotools.COCO):
+def test_modify_dataset_cat(coco_dataset: rpycocotools.COCO) -> None:
     assert coco_dataset.cats[1].name == "person"
     coco_dataset.cats[1].name = "elf"
     assert coco_dataset.cats[1].name == "elf"
 
 
-def test_set_dataset_cats(coco_dataset: rpycocotools.COCO):
+def test_set_dataset_cats(coco_dataset: rpycocotools.COCO) -> None:
     cat = rpycocotools.Category(1, "test", supercategory="super_test")
     coco_dataset.cats = {1: cat}
     assert coco_dataset.cats == {1: cat}
 
 
-def test_access_cat(coco_dataset: rpycocotools.COCO):
+def test_access_cat(coco_dataset: rpycocotools.COCO) -> None:
     assert coco_dataset.cats[2].supercategory == "vehicle"
     assert coco_dataset.cats[2].id == 2
     assert coco_dataset.cats[2].name == "bicycle"
