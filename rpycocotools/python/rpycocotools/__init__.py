f682638d8a181a9d95c63d826a46c961cf27b155
Hoel Bagard <hoel.bagard@gmail.com>
2023-05-17 18:42:24 +0900

    Fixed import order to make package work.

--- rpycocotools/python/rpycocotools/__init__.py
+++ rpycocotools/python/rpycocotools/__init__.py
@@ -1,4 +1,4 @@
 """Package providing utilities to load, manipulate, convert and visualize COCO format datasets."""
 
+from ._rpycocotools import COCO  # noqa: I001
 from . import anns, mask
-from ._rpycocotools import COCO
